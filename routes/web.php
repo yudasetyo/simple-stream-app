fd79c698f18ab57948bfc98f4eaa1465ad65adbd
Yuda Setyo Hidayat <65027692+yudasetyo@users.noreply.github.com>
2024-01-16 17:08:07 +0700

    Small change

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('/', [MovieController::class, 'index'])->name('admin.movie');
         Route::get('/create', [MovieController::class, 'create'])->name('admin.movie.create');
         Route::post('/store', [MovieController::class, 'store'])->name('admin.movie.store');
-        Route::get('/edit/{id}', [MovieController::Class, 'edit'])->name('admin.movie.edit');
-        Route::put('/update/{id}', [MovieController::Class, 'update'])->name('admin.movie.update');
+        Route::get('/edit/{id}', [MovieController::class, 'edit'])->name('admin.movie.edit');
+        Route::put('/update/{id}', [MovieController::class, 'update'])->name('admin.movie.update');
     });
 });
 
